6b35f94c8a00f60c761f13824c07da0b57e06795
rinhuiyen <38566172+rinhuiyen@users.noreply.github.com>
2019-05-25 19:01:56 +0800

    login and signup

---
+++ JomService/screens/SignUp.js
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import user from './images/user.png';
+import user1 from './images/user1.png';
+
+const { width: WIDTH } = Dimensions.get('window');
+
+export default class SignUp extends React.Component {
+  render() {
+    return (
+      <View style={styles.backgrounds}>
+        <View style={styles.logoContainer}>
+          <Text style={styles.logoText}>Login</Text>
+        </View>
+
+        <View style={styles.SectionStyle}>
+          <Image
+            source={require('./images/user1.png')}
+            style={styles.ImageStyle}
+          />
+
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Username"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <View style={styles.SectionStyle}>
+          <Image
+            source={require('./images/password.png')}
+            style={styles.ImageStyle}
+          />
+
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Password"
+            secureTextEntry={true}
+            underlineColorAndroid="transparent"
+          />
+        </View>
+
+        <View style={styles.SectionStyle}>
+          <Image
+            source={require('./images/password.png')}
+            style={styles.ImageStyle}
+          />
+
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder=" Confirm Password"
+            secureTextEntry={true}
+            underlineColorAndroid="transparent"
+          />
+        </View>
+
+        <View>
+        <Text style={styles.text}>Already Sign up? Click here</Text>
+        </View>
+
+        <TouchableOpacity style={styles.btnSignUp}>
+          <Text style={styles.btntext}>Sign Up</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
+  logoText: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: '500',
+    marginTop: 10,
+    opacity: 0.5,
+  },
+  
+  SectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: .5,
+    borderColor: '#000',
+    height: 40,
+    borderRadius: 5 ,
+    margin: 10
+},
+ 
+ImageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode : 'stretch',
+    alignItems: 'center'
+},
+btnSignUp: {
+  width: WIDTH -55,
+  height: 45,
+  borderRadius: 25,
+  backgroundColor: '#607446',
+  marginTop: 20,
+  marginHorizontal: 25
+},
+btntext: {
+  paddingTop: 10,
+  color: 'rgba(255, 255, 255, 0.7)',
+  fontSize: 16,
+  textAlign: 'center'
+},
+text: {
+  marginHorizontal: 15
+}
+});
