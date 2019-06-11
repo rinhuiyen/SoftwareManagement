cb7abaa15db4bf68413047c4e5b6d7c2142930b3
JinnyVanille <36944960+JinnyVanille@users.noreply.github.com>
2019-06-11 22:59:24 +0800

    For profile

--- JomService/screens/Message.js
+++ JomService/screens/Message.js
@@ -2,6 +2,46 @@ import React from 'react';
 
 export default class Message extends React.Component {
     render(){
-        
+        return() {
+            <View style={{height:667, backgroundColor:"#432577"}}>
+                <View style={styles.mainbody}>
+
+                    <Image style={styles.imgprofile} source={require("../assets/images/profile.png")} />
+                    <Text style={styles.name}>
+                        BTS JIN DK
+                    </Text>
+                    <Text style={styles.username}>
+                        @BTSJINDK
+                    </Text>
+
+                    <View style={styles.itemprofile}>
+                        <Image style={styles.imgitem} source={require("../assets/images/name.png")} />
+                        <Text style={styles.labelitem}>
+                            Name:
+                        </Text>
+                        <Text style={styles.labelitem}>
+                            BTSJINDK
+                        </Text>
+                    </View> 
+
+                     <View style={styles.itemprofile}>
+                        <Image style={styles.imgitem} source={require("../assets/images/contact.png")} />
+                        <Text style={styles.labelitem}>
+                            Contact Number:
+                        </Text>
+                        <Text style={styles.labelitem}>
+                            016-2244556
+                        </Text>
+                    </View>    
+
+                    
+
+
+               
+                </View>
+            </View>
+            
+        }
     }
 }
+
