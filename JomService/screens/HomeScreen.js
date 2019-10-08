ecb54bb8cea4f2bfbf517aeb7069b23f98f5f275
wf <weifan2647@gmail.com>
2019-10-09 00:12:59 +0800

    update expo, react SDK. Fix import expo libraries

--- JomService/screens/HomeScreen.js
+++ JomService/screens/HomeScreen.js
@@ -9,7 +9,7 @@ import {
   View,
   Dimensions,
 } from 'react-native';
-import { Constants } from 'expo';
+import Constants from 'expo-constants';
 import logo from './images/logo.png';
 
 const { width: WIDTH } = Dimensions.get('window');
@@ -26,11 +26,11 @@ export default class HomeScreen extends React.Component {
           <Image source={logo} style={styles.logo} />
         </View>
         <TouchableOpacity style={styles.btnLogin}
-        onPress={() => this.props.navigation.navigate('login')}>
+          onPress={() => this.props.navigation.navigate('login')}>
           <Text style={styles.text}>Login</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.btnLogin}
-        onPress={() => this.props.navigation.navigate('signUp')}>
+          onPress={() => this.props.navigation.navigate('signUp')}>
           <Text style={styles.text}>Sign Up</Text>
         </TouchableOpacity>
       </View>
@@ -54,24 +54,24 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   btnLogin: {
-  width: WIDTH -55,
-  height: 45,
-  borderRadius: 25,
-  backgroundColor: '#432577',
-  marginTop: 10,
-  marginHorizontal: 25
-},
-text: {
-  paddingTop: 10,
-  color: 'rgba(255, 255, 255, 0.7)',
-  fontSize: 16,
-  textAlign: 'center'
-},
-logo: {
-  width: 300,
-  height: 300,
-},
-logoContainer: {
-  alignItems: 'center',
-},
+    width: WIDTH - 55,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: '#432577',
+    marginTop: 10,
+    marginHorizontal: 25
+  },
+  text: {
+    paddingTop: 10,
+    color: 'rgba(255, 255, 255, 0.7)',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
 });
--- JomService/screens/Message.js
+++ JomService/screens/Message.js
@@ -8,18 +8,18 @@ import {
     ScrollView,
     AsyncStorage
 } from 'react-native';
-import { Constants } from 'expo';
+import Constants from 'expo-constants';
 
 const { width: WIDTH } = Dimensions.get('window');
 
 export default class Message extends React.Component {
     static navigationOptions = {
         title: 'Job Details',
-      }
-    state={
+    }
+    state = {
         screenHeight: 0,
-        list : this.props.navigation.getParam('item'),
-        
+        list: this.props.navigation.getParam('item'),
+
     }
     onContentSizeChange = (contentHeight) => {
         this.state.screenHeight = 0
@@ -27,37 +27,38 @@ export default class Message extends React.Component {
         this.setState({ screenHeight: contentHeight });
     };
     componentWillMount() {
-        AsyncStorage.getItem('nameDB').then((nameJSON) =>{
-          this.setState({
-            nameStored: JSON.parse(nameJSON)
-          })
+        AsyncStorage.getItem('nameDB').then((nameJSON) => {
+            this.setState({
+                nameStored: JSON.parse(nameJSON)
+            })
         })
-      }
-    render(){
+    }
+    render() {
         const { params } = this.props.navigation.state;
         const message = params ? params.message : null;
-        return(
-                <View style={styles.views}>
-                    <ScrollView 
+        return (
+            <View style={styles.views}>
+                <ScrollView
                     style={{ flex: 1 }}
                     onContentSizeChange={this.onContentSizeChange}>
                     <Text style={styles.logoText}>Thanks for accepting!</Text>
                     <Text style={styles.logoText1}>You can view your job details here:</Text>
-                   <View style={styles.element}>
-                       <Text style={styles.containerTitle}>
-                        Title: {this.state.list.title}
+                    <View style={styles.element}>
+                        <Text style={styles.containerTitle}>
+                            Title: {this.state.list.title}
                         </Text>
                         <Text style={styles.text}>
-                        Description: {this.state.list.job}
+                            Description: {this.state.list.job}
                         </Text>
                         <Text style={styles.prices}>
-                        Price: RM {this.state.list.price}
+                            Price: RM {this.state.list.price}
                         </Text>
                     </View>
                     <View
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 3,}}
+                        style={{
+                            borderBottomColor: 'black',
+                            borderBottomWidth: 3,
+                        }}
                     />
                     <Text style={styles.logoText1}>Contact Client:</Text>
 
@@ -75,11 +76,11 @@ export default class Message extends React.Component {
                             Andrew
                         </Text>
                         <Text style={styles.sublabelitem}>
-                        {this.state.nameStored}
+                            {this.state.nameStored}
                         </Text>
                     </View>
 
-                     <View style={styles.itemprofile}>
+                    <View style={styles.itemprofile}>
                         <Image style={styles.imgitem} source={require("./images/contact.png")} />
                         <Text style={styles.labelitem}>
                             Contact Number:
@@ -88,56 +89,56 @@ export default class Message extends React.Component {
                             016-22334455
                         </Text>
                         <Text>{this.state.nameStored}</Text>
-                    </View>    
-                    </ScrollView> 
-                </View>
-            
+                    </View>
+                </ScrollView>
+            </View>
+
         )
     }
 }
 
 const styles = StyleSheet.create({
-    
-    imgprofile:{
+
+    imgprofile: {
         marginLeft: 150,
-        marginTop:50,
-        height:100,
-        width:100,
+        marginTop: 50,
+        height: 100,
+        width: 100,
     },
-    name:{
-        color:"#000000",
-        fontSize:22,
-        marginLeft:150,
-        marginTop:12,
+    name: {
+        color: "#000000",
+        fontSize: 22,
+        marginLeft: 150,
+        marginTop: 12,
 
     },
-    username:{
-        color:"#000000",
-        fontSize:18,
-        marginLeft:150,
-        marginTop:4,
+    username: {
+        color: "#000000",
+        fontSize: 18,
+        marginLeft: 150,
+        marginTop: 4,
 
     },
-    itemprofile:{
-        marginTop:30,
+    itemprofile: {
+        marginTop: 30,
     },
-    labelitem:{
-        marginTop:-45,
-        marginLeft:80,
-        fontSize:18,
-        color:"black",
+    labelitem: {
+        marginTop: -45,
+        marginLeft: 80,
+        fontSize: 18,
+        color: "black",
         fontWeight: 'bold'
     },
-    sublabelitem:{
-        marginTop:10,
-        marginLeft:80,
-        fontSize:16,
-        color:"#000"
+    sublabelitem: {
+        marginTop: 10,
+        marginLeft: 80,
+        fontSize: 16,
+        color: "#000"
     },
-    imgitem:{
-        marginLeft:10,
-        height:50,
-        width:50
+    imgitem: {
+        marginLeft: 10,
+        height: 50,
+        width: 50
     },
     paragraph: {
         marginTop: 20,
@@ -145,22 +146,22 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         height: 200,
         width: 200
-      },
-      logoText: {
+    },
+    logoText: {
         color: 'black',
         fontSize: 50,
         fontWeight: '500',
         marginTop: 10,
         opacity: 0.5,
         textAlign: 'center'
-      },
-      views:{
+    },
+    views: {
         flex: 1,
         justifyContent: 'center',
         paddingTop: Constants.statusBarHeight,
         padding: 8,
-      },
-      containerTitle: {
+    },
+    containerTitle: {
         fontSize: 25,
         textAlign: 'left',
         padding: 5
@@ -194,5 +195,5 @@ const styles = StyleSheet.create({
         marginTop: 10,
         opacity: 0.5,
         textAlign: 'center'
-      },
+    },
 });
